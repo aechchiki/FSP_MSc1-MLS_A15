6c870c7073482bb6bcf9c9d3655e54e83a5b9178
aechchiki <amina.echchiki@unil.ch>
2015-12-10 10:47:58 +0100

    header update

--- SNP_count-data_download-script.sh
+++ SNP_count-data_download-script.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+
+#####
+#
+#  Author: Amina Echchiki [mailto:amina.echchiki@unil.ch]
+#  Affiliation: MSc student, MLS bioinformatics, University of Lausanne
+#  Date: 2015-12-10
+#  Name: SNP_count-data_download-script.sh
+#  Purpose: Retrieve SNP count data from remote, prior to R script launching
+#  Comments: /
+#  Testing platforms: desktop Linux 3.16.0-50-generic (Debian 7.6); server Linux 2.6.32-504.el6.x86_64
+#  Shell version: GNU bash, version 4.3.11(1)-release (x86_64-pc-linux-gnu)
+#
+#####
+
+
 #get formatted SNP count data per genomic region and data for analysis
 
 mkdir ./SNP_count-data; mv SNP_count-data
