e38cfb5b348d14efa11d4a924e0e6123fec12298
aechchiki <amina.echchiki@unil.ch>
2015-12-10 10:42:13 +0100

    bash script - download data prior to R script launch

---
+++ SNP_count-data_download-script.sh
@@ -0,0 +1,13 @@
+#get formatted SNP count data per genomic region and data for analysis
+
+mkdir ./SNP_count-data; mv SNP_count-data
+wget https://copy.com/vnXBpjpowyx0k0Yv
+mv vnXBpjpowyx0k0Yv vnXBpjpowyx0k0Yv.zip
+unzip vnXBpjpowyx0k0Yv.zip 
+ls
+
+## ls should read: 
+# isolateNu6_monoRS-sort
+# isolateNu6_polyRS-sort
+# isolateRhiir2_monoRS-sort
+# isolateRhiir2_polyRS-sort
